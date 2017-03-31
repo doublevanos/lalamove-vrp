4d65bfea77e0091988c3a32f7ca90845d815ff5a
jwong <doublevanos@gmail.com>
2017-03-31 15:36:23 -0700

    Added order gen api

--- lalamove-web.py
+++ lalamove-web.py
@@ -1,5 +1,15 @@
 from flask import Flask, jsonify
 from flask import make_response
+from shipment import Shipment
+from location import Location
+from address import Address
+import random
+
+PICKUP = 1
+DELIVERY = 2
+MAX_DRIVERS = 2
+MAX_DELIVERY = 5
+
 
 app = Flask(__name__)
 
@@ -11,8 +21,16 @@ def not_found(error):
 def hello_world():
     return 'Hello Lala!'
 
-#@app.route('/api/v1.0/')
-#def
+@app.route('/api/v1.0/orders', methods=['GET'])
+def generate_orders():
+    orders = []
+
+    # Create random number of orders up to MAX_DELIVERY constant
+    # Using i as the service type/name
+    for i in range(0, random.randrange(1, MAX_DELIVERY)):
+        orders.append(Shipment(str(i)))
+
+    return jsonify({'Orders': orders})
 
 
 if __name__ == '__main__':
