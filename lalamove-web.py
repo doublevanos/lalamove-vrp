d16d64ec2cad8e0baabdbd32d583fc62095f13e5
jwong <doublevanos@gmail.com>
2017-04-03 22:21:57 -0700

    Used javascript comment vs python

--- lalamove-web.py
+++ lalamove-web.py
@@ -150,7 +150,7 @@ urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 try:
-    //conn = psycopg2.connect("dbname='jwong' user='jwong' host='localhost'")
+    #conn = psycopg2.connect("dbname='jwong' user='jwong' host='localhost'")
     conn = psycopg2.connect(
         database=url.path[1:],
         user=url.username,
