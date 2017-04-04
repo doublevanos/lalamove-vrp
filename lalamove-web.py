eaa7c893633b3daceff6f05488324622372d747f
jwong <doublevanos@gmail.com>
2017-04-03 17:35:01 -0700

    Force purging client cache

--- lalamove-web.py
+++ lalamove-web.py
@@ -20,6 +20,17 @@ orders = []
 max_orders = 0
 lock = threading.Lock();
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
 @app.errorhandler(404)
 def not_found():
     return make_response(jsonify({'error': 'Not found'}), 404)
